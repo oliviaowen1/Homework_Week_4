a6d0f976090cf0d18313f7715cc5278623e56e8a
Olivia Owen <olivia.owen@hotmail.com>
2020-12-20 11:49:05 +0000

    Added a for loop and additional variable as well as included all if and else is statements

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
 var charLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var charUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var sym = ['!', '\"', '£', '$', '%', '^', '&', '*', '>', '<', '\;', '(', ')', '+', '.', ',', '-', ':', '?', '[', ']', '_', '{', '}', '@', '\/', '\\'];
+var choices;
 
 function generatePassword() {
   // Asking how many characters the password should be //
@@ -29,32 +29,71 @@ function generatePassword() {
     var pwdNumbers = confirm("Would you like numbers in your password?");
     var pwdSymbol = confirm("Would you like special characters in your password?");
     // Alerts the user if no character types have been selected //
-    if (pwdLowercase || pwdUppercase || pwdNumbers || pwdSymbol) {
-      console.log(pwdLowercase, pwdUppercase, pwdNumbers, pwdSymbol);
-
-      
-
-    } else {
-      alert("You must choose at least one character");
+    if (pwdLowercase === false && pwdUppercase === false && pwdNumbers === false && pwdSymbol === false) {
+      alert("You must choose at least one character, please try again!");
     }
-
-
-
-
-
   } else {
     // Alert if the password is too short or too long //
     alert("Your password must be between 8 and 128 characters, please try again!")
   }
+  // If all 4 options are positive
+  if (pwdLowercase && pwdUppercase && pwdNumbers && pwdSymbol) {
+    choices = charLower.concat(charUpper, num, sym);
+  }
+  // If only 3 options are positive
+  else if (pwdLowercase && pwdUppercase && pwdNumbers) {
+    choices = charLower.concat(charUpper, num);
+  }
+  else if (pwdLowercase && pwdUppercase && pwdSymbol) {
+    choices = charLower.concat(charUpper, sym);
+  }
+  else if (pwdLowercase && pwdNumbers && pwdSymbol) {
+    choices = charLower.concat(num, sym);
+  }
+  else if (pwdUppercase && pwdNumbers && pwdSymbol) {
+    choices = charUpper.concat(num, sym);
+  }
+  // If only two options are positive
+  else if (pwdLowercase && pwdUppercase) {
+    choices = charLower.concat(charUpper)
+  }
+  else if (pwdLowercase && pwdNumbers) {
+    choices = charLower.concat(num)
+  }
+  else if (pwdLowercase && pwdSymbol) {
+    choices = charLower.concat(sym)
+  }
+  else if (pwdUppercase && pwdNumbers) {
+    choices = charUpper.concat(num)
+  }
+  else if (pwdUppercase && pwdSymbol) {
+    choices = charUpper.concat(sym)
+  }
+  else if (pwdNumbers && pwdSymbol) {
+    choices = num.concat(sym)
+  }
+  // If only one positive is selected
+  else if (pwdLowercase) {
+    choices = charLower;
+  }
+  else if (pwdUppercase) {
+    choices = charUpper;
+  }
+  else if (pwdNumbers) {
+    choices = num;
+  }
+  else if (pwdSymbol) {
+    choices = sym;
+  };
+
+  // This variable is used when the below for loop needs to find the array length.
+  var password = [];
+
+  // Start random selection variables:
+  // Random selection for all variables: 
+  for (var i = 0; i < pwdLength; i++) {
+      var pickChoices = choices[Math.floor(Math.random() * choices.length)];
+      password.push(pickChoices);
+  }
+  
 }
-// Lowercase question answer //
-
-
-// if (pwdLowercase == true) {
-//   for (var i = 0; i < l; i++) {
-//     password += characters.charAt(Math.floor(Math.random() * charLower.length));
-//   }
-// }
-// return ;
-
-
